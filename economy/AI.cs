87db8024fd54ef7aba20f1aea82938cd239b4362
Никита Новокрещенов <nidere@gmail.com>
2018-10-30 23:10:06 +0300

    json config fields

--- economy/AI.cs
+++ economy/AI.cs
@@ -5,7 +5,8 @@ namespace economy
 {
     public class AI
     {
-
+        public int a = 0; // для примера
+        public string b = "0"; // для примера
     }
 
     public static class AIs
@@ -15,6 +16,10 @@ namespace economy
         public static void GenerateAIs()
         {
             AI ai = new AI();
+
+            ai.a = 1; // для примера
+            ai.b = "2"; // для примера
+
             ais.Add(ai);
         }
 
